ec658278b6560a9817f2b049b8421ab584745db5
Davi Silva <davisilvaphoto@gmail.com>
2024-11-19 10:31:55 -0300

    add logs

--- src/main/java/application/createUrlShortner/validators/RequestValidator.java
+++ src/main/java/application/createUrlShortner/validators/RequestValidator.java
@@ -1,15 +1,18 @@
 package application.createUrlShortner.validators;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
 public class RequestValidator {
 
     public static Map<String, String> parseRequestBody(String body, ObjectMapper objectMapper) {
         try {
             return objectMapper.readValue(body, Map.class);
         } catch (Exception exception) {
+            log.error("Error parsing request body: {}", exception.getMessage());
             throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
         }
     }
@@ -18,6 +21,7 @@ public class RequestValidator {
         try {
             return Long.parseLong(expirationTime);
         } catch (NumberFormatException e) {
+            log.error("Error parsing expiration time: {}", e.getMessage());
             throw new IllegalArgumentException("Invalid expiration time format", e);
         }
     }
